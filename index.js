a5ac32682ad9f06018093a502b11386f036226fa
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 <sharath.yaragatti@yahoo.com>
2024-07-29 12:36:56 +0530

    some changes

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
   res.status(200).send("Hello from API server");
 });
 app.get(
-  "https://sharat9703.github.io/timestamp/api/:date",
+  "/api/:date",
   function (req, res, next) {
     const dateparam = req.params.date;
     if (!dateparam) {
